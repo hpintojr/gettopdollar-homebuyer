4bbdc10a4d26abbb8834ff88ffce1a206faaa9ee
xbloc2 <hamilton@advantagefirst.com>
2025-10-27 18:06:08 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true }
+  // The 'experimental' block is removed as 'appDir: true' is the default in Next.js 14
 };
 module.exports = nextConfig;
